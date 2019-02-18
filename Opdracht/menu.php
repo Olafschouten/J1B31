bb096afec9a9859aa92472068684d8708c3b75e9
Olafschouten <olafschouten99@gmail.com>
2019-02-18 13:14:30 +0100

    opdracht php

---
+++ Opdracht/menu.php
@@ -0,0 +1,12 @@
+<div class="head">
+
+</div>
+
+<div class="nav">
+    <a href="#">Home</a>
+    <a href="labs.php">Labs</a>
+    <a href="opdracht.php">opdracht</a>
+    <a href="lab1.php">Lab 1</a>
+</div>
+
+<footer>© Olaf Schouten</footer>
